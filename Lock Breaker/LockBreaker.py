6b9b2b8d66c400f159a2e4e792b0fc05749abb8e
kasra kazemi <kasrakazemi62@gmail.com>
2023-09-07 10:28:56 +0330

    add Lock Breaker

--- Lock Breaker/LockBreaker.py
+++ Lock Breaker/LockBreaker.py
@@ -1,93 +1,68 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[128]:
-
-
 import collections
 
+# Define a function to cycle characters in a string
 def cycle(s, n):
     d = collections.deque(s)
     d.rotate(n)
     return "".join(d)
 
-number= '1234'
-
-def big_add(num1,num2):
-    answer= ""
+# Define a function for big integer addition
+def big_add(num1, num2):
+    answer = ""
     carry = 0 
-    while (num1 !="")or (num1 !="") or (carry !=0):
-        if (num1 !="") and (num2 != ""):
-            temp= int(num1[-1])+ int(num2[-1])+ carry
-        if (num1=="")and (num2==""):
-            temp= carry
-        if (num1 =="") and (num2 !=""):
-            if carry ==0:
-                answer = num2+answer
+    
+    while (num1 != "") or (num2 != "") or (carry != 0):
+        if (num1 != "") and (num2 != ""):
+            temp = int(num1[-1]) + int(num2[-1]) + carry
+        if (num1 == "") and (num2 == ""):
+            temp = carry
+        if (num1 == "") and (num2 != ""):
+            if carry == 0:
+                answer = num2 + answer
                 break
-            temp = int(num2[-1])+ carry
-        if (num1 !="")and (num2 ==""):
-            if carry ==0:
-                answer= num1+ answer
+            temp = int(num2[-1]) + carry
+        if (num1 != "") and (num2 == ""):
+            if carry == 0:
+                answer = num1 + answer
                 break
-            temp= int(num1[-1])+ carry
-        answer= str(temp%10)+answer
-        carry= int(temp/10)
+            temp = int(num1[-1]) + carry
+        
+        answer = str(temp % 10) + answer
+        carry = int(temp / 10)
         num1 = num1[:-1]
         num2 = num2[:-1]
-    return (answer)
+    
+    return answer
 
+# Input two numbers as strings
+number1 = input()
+number2 = input()
 
-# In[178]:
+# Remove spaces from input numbers
+number1 = number1.replace(" ", "")
+number2 = number2.replace(" ", "")
 
+SumResult = 1
 
-number1= (input())
-number2 = (input())
-number1=number1.replace(" ",'')
-number2=number2.replace(" ",'')
-
-
-# In[179]:
-
-
-SumResult= 1
-for i in range (len(number1)+1):
-    number1Rotaded= cycle(number1,-i)
-  
-    for j in range (len(number)+1):
-        number2Rotaded= cycle(number2,j)
-        Sum = big_add(number1Rotaded[1:4],number2Rotaded[1:4])[1:]
+# Iterate through rotations of number1
+for i in range(len(number1) + 1):
+    number1Rotated = cycle(number1, -i)
+    
+    # Iterate through rotations of number2
+    for j in range(len(number2) + 1):
+        number2Rotated = cycle(number2, j)
+        
+        # Calculate the sum of the first three digits from the rotated numbers
+        Sum = big_add(number1Rotated[1:4], number2Rotated[1:4])[1:]
         SumResult = int(Sum)
-        #print(SumResult)
-        if int(Sum)%6==0:
+        
+        # Check if the sum is divisible by 6
+        if int(Sum) % 6 == 0:
             print('Boro joloo :)')
             break
-#         if SumResult%6!=0:
-#             print('Gir oftadi :(')
-#             break
-
-    if SumResult%6!=0:
+    
+    # If the sum is not divisible by 6, print "Gir oftadi :("
+    if SumResult % 6 != 0:
         print('Gir oftadi :(')
-        
-    break
-
-         
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+    
+    break  # Exit the loop after the first iteration
---
+++ Lock Breaker/README.md
@@ -0,0 +1,47 @@
+# LockBreaker
+
+## Introduction
+
+LockBreaker is a Python code designed to break a safebox lock based on specific conditions outlined in a programming question from the Quera website. The code implements algorithms for rotating and checking combinations of digits in a safebox lock to determine if it can be unlocked under given conditions.
+
+## Problem Statement
+
+The code solves a programming challenge detailed in the Quera problem statement, which involves breaking a safebox lock by satisfying certain conditions. These conditions are related to rotating the lock's digits, performing additions, and checking divisibility by 6.
+
+For detailed information on the problem statement, you can refer to the Quera website: [Problem Statement](https://quera.org/problemset/123801/)
+
+## Code Functionality
+
+The LockBreaker code includes the following key components:
+
+1. **Cycle Function**: This function rotates characters in a string to simulate changing the lock's combination.
+
+2. **Big Addition Function**: This function performs addition on large integers represented as strings, considering carry values.
+
+3. **Main Logic**: The main logic iterates through possible combinations of lock digits, checks conditions specified in the problem statement, and determines if the lock can be successfully unlocked.
+
+## Usage
+
+To use the LockBreaker code:
+
+1. Clone or download this repository to your local machine.
+
+2. Open a terminal or command prompt and navigate to the folder containing the code.
+
+3. Run the code by executing the Python script.
+
+4. Follow the on-screen prompts to provide input (two numbers), and the code will attempt to unlock the safebox lock based on the problem's conditions.
+
+## Sample Usage
+
+You can use the provided sample input and run the code to see how it works. The code will output either "Boro joloo :)" or "Gir oftadi :(" based on whether it can unlock the lock under the given conditions.
+
+## Contributions
+
+Contributions to this project are welcome! If you have suggestions for improvements, bug fixes, or want to enhance its functionality, please feel free to open a pull request or an issue.
+
+## Acknowledgments
+
+The LockBreaker code was developed to address a specific programming challenge on the Quera website. Thanks to Quera for providing the problem statement and opportunity to solve it.
+
+Happy coding, and enjoy breaking the lock with LockBreaker!
